d6de295bf6f8b4fab9561b84795e6b5d79655003
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2019-11-11 16:06:22 +0800

    cookie monster

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import puppeteer from 'puppeteer';
 
+const HASHTAG = 'streak';
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({
+    headless: false,
+    ignoreHTTPSErrors: true,
+  });
 
   const page = await browser.newPage();
 
-  await page.goto('https://google.com');
+  await page.setRequestInterception(true);
+  page.on('request', interceptedRequest => {
+    if (
+      interceptedRequest
+        .url()
+        .startsWith(`https://hashtagify.me/data/tags/${HASHTAG}`)
+    ) {
+      // console.log(interceptedRequest.url());
+      // console.log(interceptedRequest.headers());
+    }
+    interceptedRequest.continue();
+  });
+  await page.goto(`https://hashtagify.me/hashtag/${HASHTAG}`);
+  const cookies = await page.cookies();
+  console.log(
+    cookies.filter(c => c.name === '_hashtagify-pro_session').map(a => a.value)[0],
+  );
 
   browser.close();
 })();
